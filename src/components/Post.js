77cdfed15c54d3f9498bca39b32138120a818b8e
Lars Poetzel <lars.poetzel@gmail.com>
2022-10-26 12:27:25 +0200

    added location to post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,12 @@
 const Post = ({post}) => {
     return (
         <article>
-            <h2>{post.datum}</h2>
+            <h2 id="datum">{post.datum}</h2>
+            <h3 id="von-bis">Dauer: {post.von} Uhr - {post.bis} Uhr</h3>
+            <br/>
             <p>{post.thema}</p>
-            <p>Von:{post.von} bis:{post.bis}</p>
+            <br/>
+            <h3 id="adress">{post.strasse_nr}, {post.plz} Berlin</h3>
         </article>
     )
 }
