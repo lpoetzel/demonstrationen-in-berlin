a441034e55288dd9fa27d4fb611062df6f2980ef
AndreiLhu <leahu.andreilucian@gmail.com>
2022-10-31 15:51:37 +0100

    adding calendar unfinished

---
+++ src/components/Popup.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+
+const Popup = props => {
+  return (
+    <div className="popup-box">
+      <div className="box">
+        <button className="close-icon" onClick={props.handleClose}>Close</button>
+        {props.content}
+      </div>
+    </div>
+  );
+};
+
+
+export default Popup;
