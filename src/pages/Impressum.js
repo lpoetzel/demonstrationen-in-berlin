d547b100713a8a0d23d7074e0ed5dfd79d36c23d
Lars Poetzel <lars.poetzel@gmail.com>
2022-11-03 11:43:26 +0100

    fixed ugly desktop, added text

--- src/pages/Impressum.js
+++ src/pages/Impressum.js
@@ -15,33 +15,44 @@ const Impressum = () => {
           alt="lizenz-bild"
         />{" "}
         <a href="https://creativecommons.org/licenses/by-nc/3.0/de/">Lizenz</a>.
-        Dabei wurde der Datensatz{" "}
+        Dabei wurde der Datensatz,{" "}
         <a href="https://daten.berlin.de/datensaetze/versammlungen-im-land-berlin">
           Versammlungen im Land Berlin
-        </a>{" "}
-        genutzt.{" "}
+        </a>{""}
+         , der Stadt Berlin genutzt und unverändert wieder gegeben.{" "}
       </span>{" "}
       <h3 className="impressum-header">Die Macher</h3>
       <span className="block-text">
         Im Rahmen des Trainings zum Full-Stack Developer in der{" "}
         <a href="https://www.wildcodeschool.com/">Wild Code School</a> entstand
-        dieses Project als erstes unter Nutzung von React. <br />
+        dieses Project als erstes unter Nutzung von React.
         Die Schöpfer sind: <br />
         <div className="names">
-          <div>Lars Poetzel{" "}
-          <a className="envelope" href="mailto: lars.poetzel@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope} />{" "}
-          </a></div>
+          <div>
+            Lars Poetzel{" "}
+            <a className="envelope" href="mailto: lars.poetzel@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
+            </a>
+          </div>
           <br />
-          <div> Leahu Andrei Lucian{" "}
-          <a className="envelope" href="mailto: leahu.andreilucian07@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope} />{" "}
-          </a></div>
+          <div>
+            {" "}
+            Leahu Andrei Lucian{" "}
+            <a
+              className="envelope"
+              href="mailto: leahu.andreilucian07@gmail.com"
+            >
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
+            </a>
+          </div>
           <br />
-          <div> Mathieu Durimel{" "}
-          <a className="envelope" href="mailto: mathieu.durimel@gmail.com">
-            <FontAwesomeIcon icon={faEnvelope} />{" "}
-          </a></div>
+          <div>
+            {" "}
+            Mathieu Durimel{" "}
+            <a className="envelope" href="mailto: mathieu.durimel@gmail.com">
+              <FontAwesomeIcon icon={faEnvelope} />{" "}
+            </a>
+          </div>
           <br />
         </div>
       </span>
