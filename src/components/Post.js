8844d95ee7f2a131db17896dc6de1fb663735fd4
Lars Poetzel <lars.poetzel@gmail.com>
2022-11-04 11:27:10 +0100

    removed map button, added button for "strecke"

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 const Post = ({ post }) => {
+  const [show, setShow] = useState(false);
   const street = `${post.strasse_nr}`;
-  const display = street.length ? (`${street}, ${post.plz} Berlin`) : `${post.aufzugsstrecke}`;
+  const display = street.length
+    ? <div>{street}, {post.plz} Berlin <a
+    id="maps-link"
+    href={`https://www.google.com/maps/search/?api=1&query=${street}`}
+    target="_blank"
+    rel="noreferrer"
+  >
+    Auf der Karte ansehen
+  </a></div>
+    : <div>{post.aufzugsstrecke.slice(0, 30)}...
+    <div>
+      {
+        show?<div><h3>{post.aufzugsstrecke.slice(30)}</h3><br/></div>:null
+      }
+        <button id="strecke-popup" onClick={() => setShow(!show)}>Gesamte Strecke</button>
+      </div>
+      </div>
   return (
     <article>
       <h2 id="datum">{post.datum}</h2>
@@ -10,8 +29,9 @@ const Post = ({ post }) => {
       <br />
       <p>{post.thema}</p>
       <br />
-      <h3 id="adress">{display}</h3>
-      <a id="maps-link"href={`https://www.google.com/maps/search/?api=1&query=${display}`} target="_blank" rel="noreferrer">Auf der Karte ansehen</a>
+      <h3 >{display}</h3>
+      <button id="none"/>
+      {/* what else so it does pop out properly? */}
     </article>
   );
 };
