cb0670a2c37cc50158fbf99b9ca4b3fddd8e3a92
AndreiLhu <leahu.andreilucian@gmail.com>
2022-11-11 15:44:19 +0100

    macking upbutton transparent

--- src/components/ScroolButton/Styles.js
+++ src/components/ScroolButton/Styles.js
@@ -13,7 +13,7 @@ export const Button = styled.div`
   font-size: 2.5rem;
   z-index: 1;
   cursor: pointer;
-  color: rgb(245, 245, 245);
+  color: rgb(245, 245, 245, 0.6);
 
   @media (max-width: 576px) {
     font-size: 2rem;
